c642dd19a21cc009e3486fc7a28cf734a06f90de
jothika-datasirpi <jothika.s@datasirpi.com>
2024-02-14 10:00:41 +0530

    custom-directive

---
+++ custom-directive/index.js
@@ -0,0 +1,17 @@
+//Custom directives
+var vm = new Vue({  
+    el: '#directive',  
+    data: {  
+    },  
+    methods : {  
+    },  
+ });  
+
+ //Pass value to custom directives
+ var vm = new Vue({  
+    el: '#directive1',  
+    data: {  
+    },  
+    methods : {  
+    },  
+ })  
